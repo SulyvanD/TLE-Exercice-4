c9b660135e2d85635bafb5c5bbf64369bc821d33
SulyvanD <sulyvan.debraize@net-c.com>
2018-12-18 09:18:22 +0100

    Exercice completé

---
+++ TLE - Exercice 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLE___Exercice_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shopping = new List<string>();
+            shopping.Add("Carottes");
+            shopping.Add("Oignons");
+            shopping.Add("Pommes de terre");
+            shopping.Add("Salade");
+            shopping.Add("Tomate");
+
+            foreach (string item in shopping)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
